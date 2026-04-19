45a3bbd68c106a8b89a67827ef31d4a4020686fe
sukree <masjidtaram@gmail.com>
2026-04-20 00:39:55 +0700

    add site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,57 +4,57 @@ import starlight from '@astrojs/starlight';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'duanabi.com',
+  integrations: [starlight({
+      title: 'duanabi.com',
 		  logo: {
-	        src: './src/assets/duanabi-logo.png', // ถ้ามีโลโก้หน้าเว็บ
-    	  },
-      	  favicon: './src/assets/duanabi-fav.png', // ระบุตำแหน่ง favicon ที่นี่
-          locales: {
-        	root: { label: 'ไทย', lang: 'th' }
-  		  },	
+        src: './src/assets/duanabi-logo.png', // ถ้ามีโลโก้หน้าเว็บ
+      },
+      favicon: './src/assets/duanabi-fav.png', // ระบุตำแหน่ง favicon ที่นี่
+      locales: {
+        root: { label: 'ไทย', lang: 'th' }
+      },	
 		  social: [
-			{ icon: 'email', label: 'Email', href: 'mailto:mumuivi@gmail.com' },
-			{ icon: 'facebook', label: 'Facebook', href: 'https://www.facebook.com/groups/duanabi' }
+        { icon: 'email', label: 'Email', href: 'mailto:mumuivi@gmail.com' },
+        { icon: 'facebook', label: 'Facebook', href: 'https://www.facebook.com/groups/duanabi' }
 		  ],
-          sidebar: [
-              {
-                  label: 'หมวดหมู่ดุอาอ์',
-                  items: [
-                  { label: 'อัซการฺ ยามเช้า-เย็น', autogenerate: { directory: 'azkar' } },
-                  { label: 'วิริตหลังละหมาด', autogenerate: { directory: 'wirid' } },
-                  { label: 'ดุอาอ์ให้พ่อแม่', autogenerate: { directory: 'parents' } },
-                  { label: 'ดุอาอ์ให้ลูก', autogenerate: { directory: 'children' } },
-                  ],
-              },
-              // ถ้าในอนาคตนึกหมวดใหม่ได้ ก็แค่มาเพิ่มบรรทัดใหม่ตรงนี้
-          ],
-		  head: [
-					// ตั้งค่ารูปภาพสำหรับแชร์ (แนะนำขนาด 1200x630 px)
-					{
-					tag: 'meta',
-					attrs: { property: 'og:image', content: 'https://duanabi.com/dua1.webp' },
-					},
-					{
-					tag: 'meta',
-					attrs: { property: 'og:title', content: 'Dua Nabi ดุอาอฺ ฉบับเข้าใจง่าย' },
-					},
-					{
-					tag: 'meta',
-					attrs: { property: 'og:description', content: 'รวมบทดุอาอ์จากอัลกุรอานและซุนนะฮ์ เรียนรู้ดุอาอ์พร้อมคำแปลและคำอธิบายที่ถูกต้อง' },
-					},
-			],
-
-          customCss: ['./src/styles/custom.css'],
-          components: {
-              Footer: './src/components/BottomNav.astro',
+      sidebar: [
+          {
+              label: 'หมวดหมู่ดุอาอ์',
+              items: [
+              { label: 'อัซการฺ ยามเช้า-เย็น', autogenerate: { directory: 'azkar' } },
+              { label: 'วิริตหลังละหมาด', autogenerate: { directory: 'wirid' } },
+              { label: 'ดุอาอ์ให้พ่อแม่', autogenerate: { directory: 'parents' } },
+              { label: 'ดุอาอ์ให้ลูก', autogenerate: { directory: 'children' } },
+              ],
           },
-      }),
-	],
-	devToolbar: { enabled: false },
+          // ถ้าในอนาคตนึกหมวดใหม่ได้ ก็แค่มาเพิ่มบรรทัดใหม่ตรงนี้
+      ],
+		  head: [
+                // ตั้งค่ารูปภาพสำหรับแชร์ (แนะนำขนาด 1200x630 px)
+                {
+                tag: 'meta',
+                attrs: { property: 'og:image', content: 'https://duanabi.com/dua1.webp' },
+                },
+                {
+                tag: 'meta',
+                attrs: { property: 'og:title', content: 'Dua Nabi ดุอาอฺ ฉบับเข้าใจง่าย' },
+                },
+                {
+                tag: 'meta',
+                attrs: { property: 'og:description', content: 'รวมบทดุอาอ์จากอัลกุรอานและซุนนะฮ์ เรียนรู้ดุอาอ์พร้อมคำแปลและคำอธิบายที่ถูกต้อง' },
+                },
+        ],
+
+      customCss: ['./src/styles/custom.css'],
+      components: {
+          Footer: './src/components/BottomNav.astro',
+      },
+  }), sitemap()],
+    devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
   },
