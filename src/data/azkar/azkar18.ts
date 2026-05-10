2185f2570fcb8372e63d291d2f09d6f390010918
sukree <masjidtaram@gmail.com>
2026-05-10 15:31:42 +0700

    update chg to ts 05

---
+++ src/data/azkar/azkar18.ts
@@ -0,0 +1,100 @@
+export const duaData = {
+  "title": "18-ดุอาอ์ยามเช้า(ขอความช่วยเหลือและทางนำของวัน)",
+  "fullArabic": `\
+<span class="morning"><span>☀️</span>أَصْبَحْنَا وَ أَصْبَحَ<span>☀️</span></span>
+(<span class="evening"><span>🌙</span>أَمْسَيْنَا وَ أَمْسَى<span>🌙</span></span>)
+<span class="text-emerald-700">الْمُلْكُ لِلَّهِ رَبِّ الْعَالَمِينَ،</span>\
+
+<span class="text-purple-800">اللَّهُمَّ إِنِّي أَسْأَلُكَ خَيْرَ هَذَا</span>
+<span class="morning"><span>☀️</span>الْيَوْمِ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>اللَّيْلَةِ<span>🌙</span></span>)
+
+<span class="morning"><span>☀️</span>فَتْحَهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>فَتْحَهَا<span>🌙</span></span>)
+
+<span class="morning"><span>☀️</span>وَنَصْرَهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>وَنَصْرَهَا<span>🌙</span></span>)
+
+<span class="morning"><span>☀️</span>وَنُورَهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>وَنُورَهَا<span>🌙</span></span>)
+
+<span class="morning"><span>☀️</span>وَبَرَكَتَهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>وَبَرَكَتَهَا<span>🌙</span></span>)
+
+<span class="morning"><span>☀️</span>وَهُدَاهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>وَهُدَاهَا<span>🌙</span></span>)
+
+<span class="text-green-800">وَأَعُوذُ بِكَ مِنْ شَرِّ مَا </span>\
+<span class="morning"><span>☀️</span>فِيهِ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>فِيهَا<span>🌙</span></span>)
+
+<span class="text-yellow-500">وَشَرِّ مَا</span>\
+<span class="morning"><span>☀️</span>بَعْدَهُ<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>بَعْدَهَا<span>🌙</span></span>)
+<span class="text-right text-lg text-slate-500 mt-2">คำอ่าน: \
+<span class="morning"><span>☀️</span>ตอนเช้า<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>ตอนเย็น<span>🌙</span></span>)\
+</span>
+`,
+  "fullTranslation": `\
+• <span class="text-emerald-700"> เราเข้าสู่ยามเช้า</span>\
+<span class="morning"><span>☀️</span>ยามเช้านี้<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>ยามเย็นนี้<span>🌙</span></span>)\
+<span class="text-emerald-700">และอำนาจการปกครองทั้งมวลก็เข้าสู่ยามเช้าเป็นสิทธิของอัลลอฮฺ\
+  พระผู้อภิบาลแห่งสากลโลก</span>\
+
+• <span class="text-purple-800">โอ้อัลลอฮฺ แท้จริงฉันขอต่อพระองค์ท่านซึ่งความดีงามของ</span>\
+<span class="morning"><span>☀️</span>ยามเช้านี้<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>ยามเย็นนี้<span>🌙</span></span>)
+• การพิชิต(อุปสรรค) การได้รับความช่วยเหลือ รัศมี ความจำเริญ และทางนำของมัน\
+<span class="morning"><span>☀️</span>ของยามเช้านี้<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>ของยามเย็นนี้<span>🌙</span></span>)
+
+• <span class="text-green-800">และฉันขอความคุ้มครองต่อพระองค์ท่านให้พ้นจากความชั่วร้ายที่มีอยู่ใน</span>\
+<span class="morning"><span>☀️</span>เช้านี้<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>เย็นนี้<span>🌙</span></span>) \
+<span class="text-yellow-500">และความชั่วร้ายหลังจาก</span>\
+<span class="morning"><span>☀️</span>เช้านี้<span>☀️</span></span> \
+(<span class="evening"><span>🌙</span>เย็นนี้<span>🌙</span></span>)
+
+<br><span class="text-right text-lg text-slate-500 mt-2">***วิธีอ่าน: คำที่เปลี่ยนตามเวลา
+<span class="morning"><span>☀️</span>คำอ่านตอนเช้า<span>☀️</span></span> 
+(<span class="evening"><span>🌙</span>คำอ่านตอนเย็น<span>🌙</span></span>)</span>
+  `,
+  "explanation": "“เมื่อผู้ใดในหมู่พวกเจ้าอยู่ในยามเช้า เขาจงกล่าวดุอาอ์นี้ และเมื่อถึงเวลาเย็นก็ให้อำนดุอาอันี้เช่นเดียวกัน“ (หะสัน, อบูดาวูด 5084)",
+  "audio_url": "https://assets.duanabi.com/audio/azkar/azkar18s.mp3",
+  "words": [
+    { "ar": "أَصْبَحْنَا", "read": "อัศบะห์นา", "mean": "เราเข้าสู่ยามเช้า" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "أَصْبَحَ", "read": "อัศบะหะ", "mean": "ได้เข้าสู่ยามเช้า" },
+    { "ar": "الْمُلْكُ", "read": "อัลมุลกุ", "mean": "อำนาจการปกครอง" },
+    { "ar": "لِلَّهِ", "read": "ลิลลาฮิ", "mean": "เป็นสิทธิของอัลลอฮฺ" },
+    { "ar": "رَبِّ", "read": "ร็อบบิ", "mean": "พระผู้อภิบาล" },
+    { "ar": "الْعَالَمِينَ", "read": "อัลอาละมีน", "mean": "แห่งสากลโลก" },
+    { "ar": "اللَّهُمَّ", "read": "อัลลอฮุมมะ", "mean": "โอ้อัลลอฮฺ" },
+    { "ar": "إِنِّي", "read": "อินนี", "mean": "แท้จริงฉัน" },
+    { "ar": "أَسْأَلُكَ", "read": "อัสอะลุกะ", "mean": "ขอต่อพระองค์" },
+    { "ar": "خَيْرَ", "read": "ค็อยเราะ", "mean": "ความดีงาม" },
+    { "ar": "هَذَا", "read": "ฮาซา", "mean": "นี้" },
+    { "ar": "الْيَوْمِ", "read": "อัลเยามิ", "mean": "วัน" },
+    { "ar": "فَتْحَهُ", "read": "ฟัตหะฮู", "mean": "การเปิด/การพิชิตของมัน" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "نَصْرَهُ", "read": "นัศเราะฮู", "mean": "การช่วยเหลือของมัน" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "نُورَهُ", "read": "นูเราะฮู", "mean": "รัศมีของมัน" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "بَرَكَتَهُ", "read": "บะเราะกะตะฮู", "mean": "ความจำเริญของมัน" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "هُدَاهُ", "read": "ฮุดาฮู", "mean": "ทางนำของมัน" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "أَعُوذُ بِكَ", "read": "อะอูซุ บิกะ", "mean": "ฉันขอความคุ้มครองต่อพระองค์" },
+    { "ar": "مِنْ", "read": "มิน", "mean": "จาก" },
+    { "ar": "شَرِّ", "read": "ชัรริ", "mean": "ความชั่วร้าย" },
+    { "ar": "مَا", "read": "มา", "mean": "สิ่งที่" },
+    { "ar": "فِيهِ", "read": "ฟีฮิ", "mean": "ในนั้น" },
+    { "ar": "وَ", "read": "วะ", "mean": "และ" },
+    { "ar": "شَرِّ", "read": "ชัรริ", "mean": "ความชั่วร้าย" },
+    { "ar": "مَا", "read": "มา", "mean": "สิ่งที่" },
+    { "ar": "بَعْدَهُ", "read": "บะอฺดะฮู", "mean": "หลังจากมัน" }
+  ]
+}
