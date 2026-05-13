acf37dc2fad408080dc805770090f3bdfe63681b
sukree <masjidtaram@gmail.com>
2026-05-13 17:15:55 +0700

    update color 03

---
+++ src/data/wirid/wirid08-1.ts
@@ -0,0 +1,30 @@
+export const duaData = {
+  "title": "ตัสบีห์หลังละหมาด (การสรรเสริญความบริสุทธิ์ของอัลลอฮ์)",
+  "fullArabic": `\
+<span class="text-red-700">  سُبْحَانَ اللهِ  </span> 
+
+  <span class="block text-left text-lg text-slate-500">(อ่าน 33 ครั้ง)</span>
+  <span class="block text-right text-lg text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+
+  `,
+  "fullTranslation": `\
+  • <span class="text-red-700">มหาบริสุทธิ์แด่อัลลอฮ์ </span> 
+ 
+  <span class="block text-left text-lg text-slate-500">(อ่าน 33 ครั้ง)</span>
+  <span class="block text-right text-lg text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+
+  `,
+  "explanation": `\
+  รายงานโดยท่านอบู ฮุรอยเราะฮ์ (ร.ฎ.) 
+  ท่านรสูลุลลอฮ์ (ﷺ) กล่าวว่า: “ผู้ใดที่กล่าว ซุบฮานัลลอฮ์ 33 ครั้ง, อัลหัมดุลิลลาฮ์ 33 ครั้ง และอัลลอฮุอักบัร 33 ครั้ง (รวมเป็น 99) \
+  แล้วปิดท้ายให้ครบ 100 ด้วยบท ลาอิลาฮะ อิลลัลลอฮ์...\
+  บาปของเขาจะถูกลบล้าง แม้ว่ามันจะมากเท่าฟองน้ำในทะเลก็ตาม“
+  <span class="block text-right text-sm text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+  การกล่าวบทนี้เป็นการยืนยันว่าพระองค์ทรงบริสุทธิ์จากข้อบกพร่องทั้งปวง
+  `,
+  "audio_url": "https://assets.duanabi.com/audio/wirid/wirid08-1.mp3",
+  "words": [
+    { "ar": "سُبْحَانَ", "read": "ซุบฮานะ", "mean": "มหาบริสุทธิ์/ความบริสุทธิ์เป็นของ..." },
+    { "ar": "اللهِ", "read": "อัลลอฮ์", "mean": "อัลลอฮ์" }
+  ]
+}
---
+++ src/data/wirid/wirid08-3.ts
@@ -0,0 +1,30 @@
+export const duaData={
+  "title": "ตัสบีห์หลังละหมาด บทที่ 3 (การสรรเสริญความยิ่งใหญ่ของอัลลอฮ์)",
+  "fullArabic": `\
+  <span class="text-blue-800">  اللهُ أَكْبَرُ</span>
+  <span class="block text-left text-lg text-slate-500">(อ่าน 33 ครั้ง)</span>
+  <span class="block text-right text-lg text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+
+  `,
+  "fullTranslation": `\
+<span class="text-blue-800">อัลลอฮ์ทรงยิ่งใหญ่ที่สุด</span>
+<span class="block text-left text-lg text-slate-500">(อ่าน 33 ครั้ง)</span>
+<span class="block text-right text-lg text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+
+  `,
+
+  "explanation": `\
+  รายงานโดยท่านอบู ฮุรอยเราะฮ์ (ร.ฎ.) 
+  ท่านรสูลุลลอฮ์ (ﷺ) กล่าวว่า: “ผู้ใดที่กล่าว ซุบฮานัลลอฮ์ 33 ครั้ง, อัลหัมดุลิลลาฮ์ 33 ครั้ง และอัลลอฮุอักบัร 33 ครั้ง (รวมเป็น 99) \
+  แล้วปิดท้ายให้ครบ 100 ด้วยบท ลาอิลาฮะ อิลลัลลอฮ์...\
+  บาปของเขาจะถูกลบล้าง แม้ว่ามันจะมากเท่าฟองน้ำในทะเลก็ตาม“
+  <span class="block text-right text-sm text-slate-500">(อ้างอิงจากฮะดีษ ซอเฮียะมุสลิม หมายเลข597)</span>
+  การกล่าวบทนี้เป็นการยืนยันว่าพระองค์ทรงบริสุทธิ์จากข้อบกพร่องทั้งปวง
+  `,
+
+  "audio_url": "https://assets.duanabi.com/audio/wirid/wirid08-3.mp3",
+  "words": [
+    { "ar": "اللهُ", "read": "อัลลอฮุ", "mean": "อัลลอฮ์" },
+    { "ar": "أَكْبَرُ", "read": "อักบัร", "mean": "ทรงเกริกไกรยิ่ง/ทรงยิ่งใหญ่ที่สุด" }
+  ]
+}
